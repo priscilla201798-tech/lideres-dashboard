192eaf97105d5ec09421df1623f8f2cdd3e384b9
priscilla201798-tech <priscilla2017.98@gmail.com>
2026-02-22 02:04:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title("Funciona")
-st.write("Si ves esto, el problema NO es Streamlit.")
+st.title("Prueba Secrets")
+
+if "gcp_service_account" in st.secrets:
+    st.success("Secret cargado correctamente")
+else:
+    st.error("Secret NO cargado")
